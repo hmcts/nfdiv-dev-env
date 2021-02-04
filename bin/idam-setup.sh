fd7cbc5965c0d48ffb8e150de35406d1b43de934
Nitin Prabhu <prabhunitin61@gmail.com>
2021-02-04 15:32:45 +0000

    Added roles to caseworker used in functional tests

--- bin/idam-setup.sh
+++ bin/idam-setup.sh
@@ -37,9 +37,8 @@ curl -s -o /dev/null -XPUT "${HEADERS[@]}" ${IDAM_URI}/services/${CLIENT_ID}/rol
 
 echo "Creating idam users"
 ./bin/idam-create-user.sh citizen,claimant $IDAM_CITIZEN_USERNAME $IDAM_CITIZEN_PASSWORD citizens
-./bin/idam-create-user.sh caseworker,caseworker-divorce $IDAM_CASEWORKER_USERNAME $IDAM_CASEWORKER_PASSWORD caseworker
+./bin/idam-create-user.sh caseworker,caseworker-divorce,caseworker-divorce-courtadmin_beta,caseworker-divorce-systemupdate,caseworker-divorce-courtadmin,caseworker-divorce-bulkscan,caseworker-divorce-superuser,caseworker-divorce-courtadmin-la $IDAM_CASEWORKER_USERNAME $IDAM_CASEWORKER_PASSWORD caseworker
 ./bin/idam-create-user.sh caseworker,caseworker-divorce,caseworker-divorce-courtadmin_beta $IDAM_TEST_CASEWORKER_USERNAME $IDAM_TEST_CASEWORKER_PASSWORD caseworker
 ./bin/idam-create-user.sh caseworker,caseworker-divorce,caseworker-divorce-solicitor,caseworker-divorce-superuser $IDAM_TEST_SOLICITOR_USERNAME $IDAM_TEST_SOLICITOR_PASSWORD caseworker
 ./bin/idam-create-user.sh ccd-import $CCD_DEFINITION_IMPORTER_USERNAME $CCD_DEFINITION_IMPORTER_PASSWORD Default
-
 echo "Idam setup complete"
