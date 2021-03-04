9de30550ae8f551cf7b85303977252f9318d16b7
Linus Norton <linusnorton@gmail.com>
2021-03-04 06:46:06 +0000

    fixes

--- bin/init.sh
+++ bin/init.sh
@@ -7,12 +7,12 @@ fi
 
 API_DIR=./nfdiv-case-api
 FE_DIR=./nfdiv-frontend
-SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal
+export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal
 
 az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 az acr login --name hmctsprivate --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 
-[[ -d API_DIR ]] || git clone git@github.com:hmcts/nfdiv-case-api.git
+[[ -d $API_DIR ]] || git clone git@github.com:hmcts/nfdiv-case-api.git
 [[ -d $FE_DIR ]] || git clone git@github.com:hmcts/nfdiv-frontend.git
 
 docker-compose stop
